d0fa10d4f8e03414d5f5393f283d476c56acffcc
João Pedro <joaopedrorafael19@gmail.com>
2022-04-26 11:31:00 -0300

    endpoint teste

--- bin/controllers/home_controller.dart
+++ bin/controllers/home_controller.dart
@@ -6,17 +6,18 @@ import 'package:shelf_router/shelf_router.dart';
 class HomeController {
   Handler get handler {
     final router = Router();
-    
+
     router.get('/', (Request request) {
-      
       String message = JsonEncoder.withIndent('  ').convert({
         'message': 'Api em desenolvimento',
-        
         'server datetime': DateTime.now().toString(),
       });
-      return Response.ok(message, headers: {
-        'content-type': 'application/json'
-      });
+      return Response.ok(message,
+          headers: {'content-type': 'application/json'});
+    });
+
+    router.get('/testhome', (Request request) {
+      return Response.ok("teste realizado com sucesso");
     });
 
     // Mount Controller

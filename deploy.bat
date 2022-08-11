76f0e316103066eb8b46e51ba5fbad2762b5d626
João Pedro Rafael Santos Silva <joaopedrorafael19@gmail.com>
2022-08-11 11:41:30 -0300

    teste

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,4 @@
-#!/bin/bash
-#Script para backup e execução de deploy dos Sites
-echo -e "Script de deploy Interativo (Ambiente Homologação)"
+ echo -e "Script de deploy Interativo (Ambiente Homologação)"
 
 docker-compose --env-file .env run -d -p 8000:8000 web
 heroku container:push web 
